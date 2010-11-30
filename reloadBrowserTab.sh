80eafc1d9f607ee229c4e5ecb0baa73c1cbc45ec
Reed Stoner <kaltekar@gmail.com>
2010-11-30 13:51:32 -0500

    Initial Commit

---
+++ reloadBrowserTab.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Script to reload a Google Chrome browser tab with the specified URL at
+# the specified reload interval
+# Created Nov 30, 2010 by Reed Stoner (kaltekar@gmail.com)
+
+tabURL="http://www.example.com/" 	# remember to include the trailing "/"
+reloadInterval=300 					# Interval in seconds
+
+# function to run the Applescript that reloads the Google Chrome tab.  
+# The Applescript searches all the tabs in the first window for the URL 
+# and then reloads that tab.
+function reloadTab () {
+/usr/bin/osascript << EOF
+set allTabs to "fasle"
+set tabID to 0
+set counter to 1
+
+tell application "Google Chrome"
+	tell window 1
+		repeat until allTabs = "true"
+			try
+				tell tab counter
+					set myURL to URL
+					if myURL is "$tabURL" then
+						set tabID to counter
+						set allTabs to "true"
+					end if
+					set counter to counter + 1
+				end tell
+			on error errmsg number errNum
+				if errNum is -1719 then
+					set allTabs to "true"
+				end if
+			end try
+		end repeat
+		if tabID is not 0 then
+			tell tab tabID
+				reload
+			end tell
+		end if
+	end tell
+end tell
+EOF
+}
+
+# Trap ctrl-c to exit cleanly.
+trap "echo \" Complete\"; exit 0" SIGINT SIGTERM
+
+# Set up an infinite loop
+loop=""
+echo "Press Ctrl-c to exit."
+until [ $loop ]; do 
+	echo Relaod
+	reloadTab
+	sleep $reloadInterval
+done
